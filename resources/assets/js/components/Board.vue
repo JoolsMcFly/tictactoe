0f9ac3e1315a1c91e45a1d92dd328dc133dcfd11
jools <jdephix@hotmail.com>
2017-05-15 11:47:49 +0200

    fix issue with diagonals

--- resources/assets/js/components/Board.vue
+++ resources/assets/js/components/Board.vue
@@ -1,15 +1,16 @@
 <template>
-                <div class="panel panel-default">
-                    <div class="panel-heading">Game board</div>
-                    <div class="panel-body">
-                        <cell v-for="cell in cells" :key="cell.index" :index="cell.index" :display="cell.display" @click.native="handleClick(cell.index)" :class="{clear: cell.index % grid_width == 0}"></cell>
-                        <p class="pull-right" v-if="isGameOver">VICTORY!</p>
-                    </div>
-                    <div class="panel-body">
-                        Number of moves: {{ moves }}<br />
-                        <button class="btn btn-primary" @click="reset">Reset</button>
-                    </div>
-                </div>
+    <div class="panel panel-default">
+        <div class="panel-heading">Game board - <button class="btn btn-primary btn-sm" @click="incSize(1)">Inc size</button> - <button class="btn btn-primary btn-sm" @click="incSize(-1)">Dec size</button></div>
+        <div class="panel-body">
+            <cell v-for="cell in cells" :key="cell.index" :index="cell.index" :display="cell.display" @click.native="handleClick(cell.index)" :class="{clear: cell.index % grid_width == 0}"></cell>
+            <p class="pull-right" v-if="isGameOver">VICTORY!</p>
+            <p class="pull-right" v-if="drawGame">DRAW!</p>
+        </div>
+        <div class="panel-body">
+            Number of moves: {{ moves }}<br />
+            <button class="btn btn-primary" @click="reset">Reset</button>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -24,6 +25,7 @@
                 grid_width: 3,
                 nb_cells: 9,
                 moves: 0,
+                drawGame: false,
                 lastCell: -1
             }
         },
@@ -43,9 +45,17 @@
                     });
                 }
             },
-            
-            reset () {
-                this.cells = [],
+
+            incSize(step) {
+                if ((this.grid_width <= 3 && step < 0) || (this.grid_width >= 9 && step > 0)) {
+                    return false
+                }
+                this.grid_width += step
+                this.nb_cells = this.grid_width * this.grid_width
+                this.reset()
+            },
+
+            reset() {
                 this.firstUserTurn = true
                 this.moves = 0
                 this.lastCell = -1
@@ -62,19 +72,22 @@
                 cell.display = this.firstUserTurn ? 'X' : 'O'
                 this.firstUserTurn = !this.firstUserTurn
                 Vue.set(this.cells, index, cell)
+                if (this.moves == this.nb_cells && !this.isGameOver) {
+                    this.drawGame = true
+                }
             },
-            
-            winsRow () {
+
+            winsRow() {
                 const row = Math.floor(this.lastCell / this.grid_width)
                 let cells = this.cells.slice(row * this.grid_width, (row + 1) * this.grid_width)
                 return this.winsAllCells(cells)
             },
-            
-            winsColumn () {
+
+            winsColumn() {
                 const col = this.lastCell % this.grid_width
                 let cells = []
-                for (let i=0; i<this.grid_width; i++) {
-                    cells.push(this.cells[i*this.grid_width + col])
+                for (let i = 0; i < this.grid_width; i++) {
+                    cells.push(this.cells[i * this.grid_width + col])
                 }
                 return this.winsAllCells(cells)
             },
@@ -83,8 +96,8 @@
                 let cells = []
                 if (this.lastCell % (this.grid_width + 1) === 0) {
                     // check top-left->bottom-right diagonal
-                    for (let i=0; i<this.nb_cells; i+= this.grid_width+1) {
-                        cells.push(this.cells[i])
+                    for (let i = 0; i < this.grid_width; i++) {
+                        cells.push(this.cells[i*(this.grid_width+1)])
                     }
                     if (this.winsAllCells(cells)) {
                         return true
@@ -93,25 +106,25 @@
                 if (this.lastCell % (this.grid_width - 1) === 0) {
                     // check top-right->bottom-left diagonal
                     cells = []
-                    for (let i=this.grid_width-1; i<this.nb_cells; i+= (this.grid_width-1)) {
-                        cells.push(this.cells[i])
+                    for (let i = 0; i < this.grid_width; i++) {
+                        cells.push(this.cells[(i+1) * (this.grid_width - 1)])
                     }
                     if (this.winsAllCells(cells)) {
                         return true
                     }
                 }
-                
+
                 return false
             },
-            
+
             winsAllCells(cells) {
                 const value_to_check = this.cells[this.lastCell].display
                 return cells.filter(function (cell) {
-                        return cell.display === value_to_check
-                    }, this).length === this.grid_width
+                    return cell.display === value_to_check
+                }, this).length === this.grid_width
             }
         },
-        
+
         computed: {
             isGameOver() {
                 if (this.lastCell === -1 || this.moves < this.grid_width * 2 - 1) {
